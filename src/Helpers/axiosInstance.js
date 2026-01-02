bf420d0c928d4533a29654e4e24eedcf66ecb1bf
Ashish Prabhakar <infotech8513@gmail.com>
2026-01-02 19:03:19 +0530

    changes

--- src/Helpers/axiosInstance.js
+++ src/Helpers/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const BASE_URL =
-  import.meta.env.VITE_API_BASE_URL || "https://api.never777skipyourwork25.qpon/api/v1";
+  import.meta.env.VITE_API_BASE_URL || "http://localhost:5000/api/v1";
 
 
 const axiosInstance = axios.create({
